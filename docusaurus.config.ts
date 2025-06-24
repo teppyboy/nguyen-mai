fb5261b54b9eff29d77d64248bf0d67ad651bf05
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-06-24 20:13:07 +0700

    chore: ignore broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   organizationName: 'teppyboy', // Usually your GitHub org/user name.
   projectName: 'nguyen-mai', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
